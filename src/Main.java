139595ab726b9194ef7a550225f84bfbb82b2c04
Alivitor <alivitor902@gmail.com>
2025-08-07 21:37:55 -0300

    Codigo COmpleto

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    //Objetivo: Converter reais para dólar.
+    //
+    //Variáveis sugeridas: double valorEmReais, double cotacaoDolar.
+    //
+    //Tarefas:
+    //
+    //    Guardar valor em reais.
+    //
+    //    Guardar cotação.
+    //
+    //    Calcular valor em dólar.
+    //
+    //    Imprimir:
+    //
+    //    R$ 500,00 equivalem a US$ 100,00
+
+    public static void main(String[] args) {
+        //Inicia scanner
+        Scanner scanner = new Scanner(System.in);
+        //Recebe valor em dinheiro
+        System.out.println("Digite o Valor em Reais");
+        Double valorEmReais = scanner.nextDouble();
+        //Recebe valor em dolar
+        System.out.println("Digite a cotação Atual do Dolar");
+        Double cotacaoDolar = scanner.nextDouble();
+        //Calcula o valor em dolar
+        Double valorEmDolar =  valorEmReais / cotacaoDolar;
+        //Apresenta somente 2 casas apos a virgula
+        valorEmDolar = Math.round(valorEmDolar * 100.0) / 100.0;
+        //Apresenta resultado
+        System.out.println( "R$" + valorEmReais + " São equivalentes á US$" + valorEmDolar );
+
+
+
+
+    }
+
+
+}
